4e1e213d3418399dd6989af27dd3432934cfeb72
Dmitry Branitskiy <dmitry.branitskiy@gmail.com>
2020-12-11 21:17:57 -0500

    Update test

--- test.js
+++ test.js
@@ -1,10 +1,24 @@
 const axios = require('axios');
+const config = require('./config.js');
 
-(async () => {
+run();
+
+//(async () => {
+//})();
+
+async function run() {
+  for (let item of config.items) {
+    console.info(item);
+  }
+}
+
+async function req(url, id) {
   try {
-    const response = await axios.get('https://www.newegg.com/product/api/ProductRealtime?ItemNumber=14-137-609&RecommendItem=&BestSellerItemList=&IsVATPrice=true')
+    const response = await axios.get('https://www.newegg.com/product/api/ProductRealtime?ItemNumber=' + id + '&RecommendItem=&BestSellerItemList=&IsVATPrice=true');
     console.log(response.data.MainItem.Instock);
   } catch (error) {
     console.log(error.response.body);
+  } finally {
+    setTimeout(req(url, id), config.interval);
   }
-})();
+}
