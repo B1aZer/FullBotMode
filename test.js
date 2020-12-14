eb68e49be7896178fae0664659d15a04f9f0da4c
Dmitry Branitskiy <dmitry.branitskiy@gmail.com>
2020-12-13 20:15:26 -0500

    update test

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 const axios = require('axios');
-//const WebSocket = require('ws');
-//const ws = new WebSocket("ws://192.168.1.18:8081");
+const WebSocket = require('ws');
+const ws = new WebSocket("ws://192.168.1.18:8081");
 const config = require('./config.js');
 const utils = require('./utils.js');
 const found = new Map();
+const moment = require('moment');
 
 run();
 
@@ -13,32 +14,40 @@ function run() {
   }
 }
 
-function constructReq(item) {
+async function constructReq(item) {
   try {
-    let itemJSON = req(item.id);
+    let itemJSON = await req(item.id);
+    if (!itemJSON) {console.error(itemJSON); return; }
     if (itemJSON.Instock) {
       if (itemJSON.UnitCost <= item.price) {
           console.info('hooray');
+	  console.log(moment().format());
         if (!found.get(item.url)) {
-          //ws.send(JSON.stringify({url: item.url, strategy: 'newegg'}));
+          ws.send(JSON.stringify({url: item.url, strategy: 'newegg'}));
           found.set(item.url, true);
         }
       } else {
         console.warn('price does not match for');
         console.info(item.url);
+	console.log(moment().format());
       }
     } else {
-      console.info('not in stock');
       found.set(item.url, false);
     }
   } catch (e) {
-    if (error.response.status == 503) {
-      console.error('Newegg 503 (service unavailable) Error. Interval possibly too low. Consider increasing interval rate.')
+    if (e.response) {
+ 	if (e.response.status == 503) {
+	  console.error('Newegg 503 (service unavailable) Error. Interval possibly too low. Consider increasing interval rate.')
+        } else {
+          console.warn(moment().format() + ': ' + e.response.status);
+        }
+    } else if (e.errno) {
+      console.warn(moment().format() + ': ' + e.errno);
     } else {
-      console.warn(e);
+      console.error(e);
     }
   } finally {
-    setTimeout(constructReq.bind(null, item), Math.random() * config.interval + config.interval);
+    setTimeout(constructReq.bind(null, item), Math.random() * config.interval +  config.interval + config.interval);
   }
 }
 
